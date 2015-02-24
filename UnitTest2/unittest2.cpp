7882abba7e7a94e05fb76d2f6ec806ef06029ecf
Liu Gantian <gantian.here@gmail.com>
2015-02-24 21:29:51 +0800

    Unit Testing 2: Added in a test to search the input content, regardless of the case. Code failed the test

--- UnitTest2/unittest2.cpp
+++ UnitTest2/unittest2.cpp
@@ -31,5 +31,29 @@ namespace UnitTest2
 
 		}
 
+		TEST_METHOD(SearchWithCaseDifferentInput)
+		{
+			// TODO: Your test code here
+
+			TextBuddy tbuddy;
+
+			tbuddy.addLine("Lily is a girl");
+			tbuddy.addLine("Zebra is an animal");
+			tbuddy.addLine("Julia is a GIRL");
+			tbuddy.addLine("Crab's daughter is a babyGirl whale");
+
+			tbuddy.searchFile("girl");
+			vector<string> actualTBList = tbuddy.getTempTextBuddy();
+
+			vector<string> expectedTBList;
+			expectedTBList.push_back("Lily is a girl");
+			expectedTBList.push_back("Julia is a GIRL");
+			expectedTBList.push_back("Crab's daughter is a babyGirl whale");
+
+			Assert::AreEqual(expectedTBList[0], actualTBList[0]);
+			Assert::AreEqual(expectedTBList[1], actualTBList[1]);
+			Assert::AreEqual(expectedTBList[2], actualTBList[2]);
+		}
+
 	};
 }
